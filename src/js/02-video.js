4be4c9cdf4c9dca5d2eb42677accbc7e7da48b03
Anatoliy Yakovenko <yakovenko.anatoliy@gmail.com>
2023-01-31 07:51:50 +0200

    complete task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+const CURRENT_TIME_KEY = 'videoplayer-current-time';
+
+const currentTime = localStorage.getItem(CURRENT_TIME_KEY)
+  ? localStorage.getItem(CURRENT_TIME_KEY)
+  : 0;
+
+function getCurrentTime(event) {
+  localStorage.setItem(CURRENT_TIME_KEY, event.seconds);
+}
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+player.setCurrentTime(currentTime);
